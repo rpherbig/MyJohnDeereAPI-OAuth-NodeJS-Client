5242442adfff1b0e9bbc226565d6ade6f0ea8430
Pravin Taralkar <TaralkarPravin@johndeere.com>
2017-03-21 12:42:31 -0500

    US57628: NodeJS File Upload sample and adding eslint

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-"use strict";
-
 const express = require('express');
 const OAuth = require('oauth').OAuth;
 const fs = require('fs-extra');
@@ -31,6 +29,20 @@ var config = {
 };
 var oAuthSession;
 var apiCatalog;
+/*
+ * Build local server to allow callback for OAuth authentication
+ */
+var app = express();
+/*
+ * Store for request and access tokens
+ */
+var tokens = {};
+
+var getLinkFrom = function (links, rel) {
+    return links.find(function (link) {
+        return rel === link.rel;
+    }).uri;
+};
 
 /*
  * 1. Call platformBaseUri with client credentials to get the API Catalog
@@ -39,34 +51,35 @@ var apiCatalog;
  * will get the URIs from the API Catalog. The get request is executed without any
  * accessToken information as we need  to get this during authentication.
  *
- * IMPORTANT: In order to get the OAuth working with the MyJohnDeere platform, it is 
+ * IMPORTANT: In order to get the OAuth working with the MyJohnDeere platform, it is
  * always necessary to set the correct Accept header as configured above!
  *
- * From the API Catalog, we need the 'oauthRequestToken', 'oauthAuthorizationRequestToken', 
+ * From the API Catalog, we need the 'oauthRequestToken', 'oauthAuthorizationRequestToken',
  * and 'oauthAccessToken' URIs to proceed.
  */
 
 oAuthSession = new OAuth(undefined, undefined, config.clientKey, config.clientSecret,
     config.oAuthVersion, config.authorizeCallbackUri, config.oAuthSignatureMethod, config.oAuthNonceSize, config.oAuthCustomHeaders);
 
-oAuthSession.get(config.platformBaseUri, null, null, function(error, responseData, result) {
+oAuthSession.get(config.platformBaseUri, null, null, function (error, responseData, result) {
 
     apiCatalog = JSON.parse(responseData);
 
     console.log('----- Getting OAuth URIs from API Catalog -----');
+    console.log('StatusCode =>' + result.statusCode);
 
-    apiCatalog.links.forEach(function(link) {
-        if (link.rel === 'oauthRequestToken') {
+    apiCatalog.links.forEach(function (link) {
+        if ('oauthRequestToken' === link.rel) {
             config.oauthRequestTokenUri = link.uri;
             return;
         }
 
-        if (link.rel === 'oauthAuthorizeRequestToken') {
+        if ('oauthAuthorizeRequestToken' === link.rel) {
             config.oauthAuthorizeRequestTokenUri = link.uri;
             return;
         }
 
-        if (link.rel === 'oauthAccessToken') {
+        if ('oauthAccessToken' === link.rel) {
             config.oauthAccessTokenUri = link.uri;
             return;
         }
@@ -77,16 +90,7 @@ oAuthSession.get(config.platformBaseUri, null, null, function(error, responseDat
     console.log('----- Ready to authenticate -----');
 });
 
-/*
- * Build local server to allow callback for OAuth authentication
- */
 
-var app = express();
-
-/*
- * Store for request and access tokens
- */
-var tokens = {};
 
 /*
  * 2. Local endpoint that is called to start the OAuth process by requesting
@@ -98,17 +102,17 @@ var tokens = {};
  * Before that, a new oAuthSession object is instantiated with the requestToken and accessToken
  * URI that we got above.
  */
-app.get('/oauth', function(req, res) {
+app.get('/oauth', function (req, res) {
     oAuthSession = new OAuth(config.oauthRequestTokenUri, config.oauthAccessTokenUri, config.clientKey, config.clientSecret,
         config.oAuthVersion, config.authorizeCallbackUri, config.oAuthSignatureMethod, config.oAuthNonceSize, config.oAuthCustomHeaders);
 
     console.log('----- Requesting Request Token and Secret -----');
 
-    oAuthSession.getOAuthRequestToken(function(error, token, secret, results) {
+    oAuthSession.getOAuthRequestToken(function (error, token, secret, results) {
         tokens.requestToken = token;
         tokens.requestTokenSecret = secret;
-
         console.log('----- Request Token and Secret Received -----');
+        console.log('StatusCode =>' + results.statusCode);
         console.log(JSON.stringify(tokens, null, 2));
 
         console.log('----- Redirecting to oauthAuthorizeRequestTokenUri -----');
@@ -123,7 +127,7 @@ app.get('/oauth', function(req, res) {
  *
  * With the verifier, access token and secret can be obtained.
  */
-app.get('/callback', function(req, res) {
+app.get('/callback', function (req, res) {
     tokens.verifier = req.query.oauth_verifier;
 
     console.log('----- Callback - Verifier Received -----');
@@ -131,11 +135,12 @@ app.get('/callback', function(req, res) {
 
     console.log('----- Requesting Access Token and Secret -----');
 
-    oAuthSession.getOAuthAccessToken(tokens.requestToken, tokens.requestTokenSecret, tokens.verifier, function(error, token, secret, results) {
+    oAuthSession.getOAuthAccessToken(tokens.requestToken, tokens.requestTokenSecret, tokens.verifier, function (error, token, secret, results) {
         tokens.accessToken = token;
         tokens.accessTokenSecret = secret;
 
         console.log('----- Access Token and Secret Received -----');
+        console.log('StatusCode =>' + results.statusCode);
         console.log(JSON.stringify(tokens, null, 2));
 
         console.log('----- Ready to do OAuth authenticated calls now-----');
@@ -146,15 +151,12 @@ app.get('/callback', function(req, res) {
 /*
  * 4. Local endpoint to demonstrate how to perform authenticate requests
  */
-app.get('/sampleRequest', function(req, res) {
+
+app.get('/sampleRequest', function (req, res) {
     console.log('----- Doing Sample Request -----');
 
-    // Get organizations URI from API catalog
-    var organizationsLink = apiCatalog.links.find(function(link) {
-        return link.rel === 'organizations';
-    });
-
-    oAuthSession.get(organizationsLink.uri, tokens.accessToken, tokens.accessTokenSecret, function(error, responseData, result) {
+    oAuthSession.get(getLinkFrom(apiCatalog.links, 'organizations'), tokens.accessToken, tokens.accessTokenSecret, function (error, responseData, result) {
+        console.log('StatusCode =>' + result.statusCode);
         console.log('----- Sample Request Response -----');
         console.log(JSON.stringify(JSON.parse(responseData), null, 2));
 
@@ -166,57 +168,64 @@ app.get('/sampleRequest', function(req, res) {
  * 5. Local endpoint to demonstrate how to perform Create File in a MyJohnDeere organization.Below endpoint just picks up the first organization
  * user has access to upload a file and creates (POST) a spot and upload (PUT) a file.
  */
-app.get('/uploadFile', function(req, res) {
-    console.log('----- Doing Create File Request -----');
-    var body = {name: 'fileName.zip'};
 
-    var uploadFile = function (error, responseData, result) {
-        if(!error) {
-            var location = result.headers.location;
-            console.log('Uploading file with =>' + location);
-            fs.readFile('./RX.zip', function (err, data) {
+var uploadFile = function (createFileError, responseData, createFileResult) {
+    if (!createFileError) {
+        var location = createFileResult.headers.location;
+        console.log('Uploading file with =>' + location);
+        fs.readFile('./RX.zip', function (fsError, data) {
+            if (!fsError) {
                 oAuthSession.put(location, tokens.accessToken, tokens.accessTokenSecret, data,
-                    'application/octet-stream', function (error, responseData, result) {
-                        console.log('File Uploaded =>' + result.statusCode)
-                        res.end();
+                    'application/octet-stream', function (uploadError, uploadResponseData, result) {
+                        if (uploadError) {
+                            console.log('Error in uploading file' + uploadError);
+                        }
+                        console.log('File Uploaded =>' + result.statusCode);
                     });
-
-            })
-        }
-        else {
-            console.log('Error in creating file'+ error)
-        }
+            }
+        });
+    } else {
+        console.log('Error in creating file' + createFileError);
     }
-    oAuthSession.get(config.platformBaseUri, tokens.accessToken, tokens.accessTokenSecret, function(error, responseData, result) {
+};
+
+function createAndUploadFile(uri) {
+    if (uri) {
+        console.log('Creating a file with =>' + uri);
+        oAuthSession.post(uri, tokens.accessToken, tokens.accessTokenSecret, JSON.stringify({name: 'fileName.zip'}),
+            'application/vnd.deere.axiom.v3+json', uploadFile);
+    } else {
+        console.log('User does not have an organization he can upload files!!');
+    }
+}
+
+function getFirstOrganizationWithFileUploadAccess(organizationResponseData) {
+    var organizations = JSON.parse(organizationResponseData).values;
+    var org = organizations.find(function (organization) {
+        var uploadFileLink = getLinkFrom(organization.links, 'uploadFile');
+        if (uploadFileLink) {
+            return true;
+        }
+        return false;
+    });
+    return org;
+}
+
+app.get('/uploadFile', function (req, res) {
+    console.log('----- Doing Create File Request -----');
+    res.end();
+    oAuthSession.get(config.platformBaseUri, tokens.accessToken, tokens.accessTokenSecret, function (catalogError, responseData, catalogResult) {
         apiCatalog = JSON.parse(responseData);
 
-        var organizationsLink = apiCatalog.links.find(function(link) {
-            return link.rel === 'organizations';
-        });
+        console.log('Get catalog StatusCode =>' + catalogResult.statusCode);
+        var organizationsLink = getLinkFrom(apiCatalog.links, 'organizations');
 
-        var fileUploadLink = function (link) {
-            return link.rel === 'uploadFile';
-        };
-        oAuthSession.get(organizationsLink.uri+';count=100', tokens.accessToken, tokens.accessTokenSecret, function(error, responseData, result) {
-            var organizations = JSON.parse(responseData).values;
+        oAuthSession.get(organizationsLink + ';count=100', tokens.accessToken, tokens.accessTokenSecret, function (error, organizationResponseData, result) {
+            console.log('get Organizations StatusCode =>' + result.statusCode);
 
-            var org = organizations.find(function (organization) {
-                var uploadFileLink = organization.links.find(fileUploadLink)
-                if(uploadFileLink) {
-                    return true
-                }
-                return false;
+            var org = getFirstOrganizationWithFileUploadAccess(organizationResponseData);
 
-            });
-            if(org) {
-                var uri = org.links.find(fileUploadLink).uri;
-                console.log('Creating a file with =>' + uri);
-                oAuthSession.post(uri, tokens.accessToken, tokens.accessTokenSecret, JSON.stringify(body),
-                    'application/vnd.deere.axiom.v3+json', uploadFile);
-            }
-            else{
-                console.log('User does not have an organization he can upload files!!')
-            }
+            createAndUploadFile(getLinkFrom(org.links, 'uploadFile'));
         });
     });
 });
